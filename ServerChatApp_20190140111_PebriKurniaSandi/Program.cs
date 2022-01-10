48ecb62c93bd7c1cfac2c2441fdece8f424614b8
PebriKurniaSandi <fksandi07@gmail.com>
2022-01-10 23:38:20 +0700

    Add project files.

---
+++ ServerChatApp_20190140111_PebriKurniaSandi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using _WCFService_2Way_20190140111;
+
+namespace ServerChatApp_20190140111_PebriKurniaSandi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostobjek = null;
+            try
+            {
+                hostobjek = new ServiceHost(typeof(ServiceCallback));
+                hostobjek.Open();
+
+                Console.WriteLine("Server On, Ready to Use");
+                Console.ReadLine();
+
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
